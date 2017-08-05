beea8f9f3ebb105db53954e5afa7f88775242433
Coolioesis <30718228+Coolioesis@users.noreply.github.com>
2017-08-06 07:45:08 +1000

    package.json changed

--- src/app/dictionary/feed.js
+++ src/app/dictionary/feed.js
@@ -2,13 +2,24 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
-app.get('/',function(req,res) {
+app.get('/',function(req,res,next) {
   res.sendFile(path.join(__dirname+'/dictionary.html'));
 });
 
+app.use(bodyParser.urlencoded( {
+  extended:true
+}));
+
+app.use(bodyParser.json());
+
+app.get('/test',function(req,res,next) {
+  res.send('hey')
+})
+
 app.post('/postWord',function(req,res,next) {
-  console.log(req.body);
+  console.log(req.body.word);
 });
 
 app.listen(3000, function() {
