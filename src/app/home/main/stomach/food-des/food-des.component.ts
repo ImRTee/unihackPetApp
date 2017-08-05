46cb290da1cefb620ac3a1931af142bb1a5ba299
ImRTee <imrtee8@gmail.com>
2017-08-06 09:18:07 +1000

    Official local one

---
+++ src/app/home/main/stomach/food-des/food-des.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-food-des',
+  templateUrl: './food-des.component.html',
+  styleUrls: ['./food-des.component.css']
+})
+export class FoodDesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
