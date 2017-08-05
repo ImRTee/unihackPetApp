66fc7ee539d4c465e3116929c0a1bb39d67da7df
ImRTee <imrtee8@gmail.com>
2017-08-06 01:59:49 +1000

    Home component and Stomach Component finished

--- src/app/quizz/quizz.component.ts
+++ src/app/quizz/quizz.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
-export class QuizzComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class QuizzComponent {
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'like';
+  defaultQuestion2 = 'going';
+  defaultQuestion3 = 'full';
+  defaultQuestion4 = 'morning';
+  suggestUserName() {
+    const suggestedName = 'Superuser';
   }
-
+    // onSubmit(form: NgForm) {
+    // console.log(form);
+    // }
+    onSubmit() {
+      console.log(this.signupForm);
+    }
 }
