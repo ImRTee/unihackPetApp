d436fdc08f24db14c725cc790731b2463d8f5b64
ImRTee <imrtee8@gmail.com>
2017-08-05 14:30:53 +1000

    Components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,26 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './src/app/authentication/authentication.component';
+import { QuizzComponent } from './src/app/quizz/quizz.component';
+import { AdoptingPhaseComponent } from './src/app/quizz/adopting-phase/adopting-phase.component';
+import { HomeComponent } from './src/app/home/home.component';
+import { FeedComponent } from './src/app/home/feed/feed.component';
+import { AdventureComponent } from './src/app/home/adventure/adventure.component';
+import { StomachComponent } from './src/app/home/stomach/stomach.component';
+import { HelpComponent } from './src/app/home/help/help.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    QuizzComponent,
+    AdoptingPhaseComponent,
+    HomeComponent,
+    FeedComponent,
+    AdventureComponent,
+    StomachComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
